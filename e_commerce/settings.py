0b0705fbd017cfabd826c02e9161cf4d43aaafdb
KavuriGreeshma2326 <kavurigreeshma1004@gmail.com>
2025-09-28 13:47:18 +0530

    Add Render domain to ALLOWED_HOSTS

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["e-commerce-backend-3-3uhi.onrender.com"]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
