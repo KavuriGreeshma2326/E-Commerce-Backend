09efd2ffb0b6caa47ca6bce24e2d7d51933e67dd
KavuriGreeshma2326 <kavurigreeshma1004@gmail.com>
2025-09-28 15:13:05 +0530

    Fix login template path

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup_view, name="signup"),
-    path('login/', auth_views.LoginView.as_view(template_name="templates/login.html"), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("products/", views.product_list, name="product_list"),
     path("products/<int:pk>/", views.product_detail, name="product_detail"),
