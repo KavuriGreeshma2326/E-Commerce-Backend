3cf9ecfb31c0f56cd03c7eb166862b857fdc0f81
KavuriGreeshma2326 <kavurigreeshma1004@gmail.com>
2025-09-28 14:35:34 +0530

    Update settings for Render deployment e-commerce-backend-6

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -69,8 +69,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"  # required for Render
+STATICFILES_DIRS = [BASE_DIR / "static"]   # Your development static files
+STATIC_ROOT = BASE_DIR / "staticfiles"     # Where collectstatic will put files
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
